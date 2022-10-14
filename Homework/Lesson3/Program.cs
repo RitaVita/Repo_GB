649acd01b0a8d866b718d020f4300c63d47b8398
RitaVita <113542732+RitaVita@users.noreply.github.com>
2022-10-14 17:14:17 +0300

    HW less3

--- Homework/Lesson3/Program.cs
+++ Homework/Lesson3/Program.cs
@@ -4,7 +4,7 @@
 12821 -> да
 23432 -> да
 
-*/
+
 
 Console.WriteLine("Введите число: ");
 string num = Console.ReadLine();
@@ -24,4 +24,27 @@ if (length == 5)
 else
 {
     Console.WriteLine("Введено неправильное число!");
+}
+
+*/
+
+
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i}, ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
